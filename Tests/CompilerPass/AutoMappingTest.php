08014f31724215ffd9dcad36d91fec8b25315d17
Matthias Devlamynck <matthias.devlamynck@rich-id.fr>
2021-08-11 15:50:50 +0200

    Add tests to check if both mapping can be used

---
+++ Tests/CompilerPass/AutoMappingTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace RichCongress\RecurrentFixturesTestBundle\Tests\CompilerPass;
+
+use RichCongress\RecurrentFixturesTestBundle\TestCase\TestCase;
+use RichCongress\RecurrentFixturesTestBundle\Tests\Resources\Entity\DummyUser;
+use RichCongress\TestFramework\TestConfiguration\Annotation\TestConfig;
+
+
+/**
+ * @covers \RichCongress\RecurrentFixturesTestBundle\DependencyInjection\CompilerPass\DoctrineMappingPass
+ */
+class AutoMappingTest extends TestCase
+{
+    /**
+     * @TestConfig("fixtures")
+     */
+    public function testCanPersistWithFixturesDatabase(): void
+    {
+        $user = new DummyUser();
+        $this->getManager()->persist($user);
+        $this->getManager()->flush();
+
+        self::assertEquals(2, $user->getId());
+    }
+
+    /**
+     * @TestConfig("kernel")
+     */
+    public function testCanPersistWithEmptyDatabase(): void
+    {
+        $user = new DummyUser();
+        $this->getManager()->persist($user);
+        $this->getManager()->flush();
+
+        self::assertEquals(1, $user->getId());
+    }
+}
