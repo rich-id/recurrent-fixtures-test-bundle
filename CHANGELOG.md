03ca49c16c696cae0b411924011e8a7b01a051b8
Nicolas Guilloux <nicolas.guilloux@protonmail.com>
2021-08-19 15:36:43 +0200

    Add SQLite REGEXP and REGEXP_REPLACE

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,33 +1,37 @@
 # Changelog
 
-# Version 0.1.8
+## Version 0.1.9
+
+- Add REGEXP and REGEXP_REPLACE in SQLite
+
+## Version 0.1.8
 
 - Add FixtureGenerator in AbstractFixture
 
-# Version 0.1.7
+## Version 0.1.7
 
 - Autoconfigure doctrine
 
-# Version 0.1.6
+## Version 0.1.6
 
 - Warn authentication failure when user has no role
 
-# Version 0.1.5
+## Version 0.1.5
 
 - Add TestAuthenticator
 
-# Version 0.1.4
+## Version 0.1.4
 
 - Add flush step on each fixtures to make sure that any dependency reference has an id
 
-# Version 0.1.3
+## Version 0.1.3
 
 - Fix a major bug around the fixtures where it failed to recognize entities
 
-# Version 0.1.2
+## Version 0.1.2
 
 - Rewrite `getReference` and `setReference` in `AbstraxtFixtures` to support the new way of declaring references
 
-# Version 0.1.1
+## Version 0.1.1
 
 - Fix composer dependencies
---
+++ src/Doctrine/Functions/Mysql/Regexp.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RichCongress\RecurrentFixturesTestBundle\Doctrine\Functions\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+final class Regexp extends FunctionNode
+{
+    public ?Node $value;
+    public ?Node $regexp;
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->value = $parser->StringPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->regexp = $parser->StringExpression();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return \sprintf(
+            '(%s REGEXP %s)',
+            $this->value->dispatch($sqlWalker),
+            $this->regexp->dispatch($sqlWalker)
+        );
+    }
+}
